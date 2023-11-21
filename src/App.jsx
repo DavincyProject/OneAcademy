ee1e841d0e44a898a11896c971cb017482206d55
Davincy Project <bdavincy.bd@gmail.comm>
2023-11-21 12:59:37 +0700

    tweaks: add toast to reset password and change devTool on staging

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,32 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
 import NotFound from "./pages/NotFound";
-import ResetPassword from "./pages/ResetPassword";
+import ResetPassword from "./pages/Auth/ResetPassword";
 import SendReset from "./components/Auth/SendReset";
 function App() {
-  return (
-    <>
-      <Provider store={store}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
+    return (
+        <>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
 
-            {/* Authorization */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/reset" element={<SendReset />} />
-            <Route path="/forgot/:id" element={<ResetPassword />} />
+                        {/* Authorization */}
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/reset" element={<SendReset />} />
+                        <Route path="/forgot/:id" element={<ResetPassword />} />
 
-            {/* Handle Pages Not Found */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </Provider>
-    </>
-  );
+                        {/* Handle Pages Not Found */}
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </BrowserRouter>
+            </Provider>
+        </>
+    );
 }
 
 export default App;
--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -4,50 +4,52 @@ import { setToken, setTokenReset } from "../reducers/authReducers";
 import toast from "react-hot-toast";
 
 export const login = (email, password, navigate) => async (dispatch) => {
-  try {
-    const response = await axios.post(ENDPOINTS.login, {
-      email,
-      password,
-    });
+    try {
+        const response = await axios.post(ENDPOINTS.login, {
+            email,
+            password,
+        });
 
-    const { data } = response.data;
-    const { token } = data;
+        const { data } = response.data;
+        const { token } = data;
 
-    dispatch(setToken(token));
-    navigate("/");
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      toast.error(`${error?.response?.data?.errors?.msg}`, {
-        duration: 2000,
-      });
-      return;
+        dispatch(setToken(token));
+        navigate("/");
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            toast.error(`${error?.response?.data?.errors?.msg}`, {
+                duration: 2000,
+            });
+            return;
+        }
+
+        toast.error(`${error?.errors}`, {
+            duration: 2000,
+        });
     }
-
-    toast.error(`${error?.errors}`, {
-      duration: 2000,
-    });
-  }
 };
 
 export const resetPassword = (email) => async (dispatch) => {
-  try {
-    const response = await axios.post(ENDPOINTS.resetpassword, {
-      email,
-    });
+    try {
+        const response = await axios.post(ENDPOINTS.resetpassword, {
+            email,
+        });
 
-    const { data } = response.data;
-    const { tokenReset } = data;
-    dispatch(setTokenReset(tokenReset));
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      toast.error(`${error?.response?.data?.errors?.msg}`, {
-        duration: 2000,
-      });
-      return;
+        const { data } = response.data;
+        const { tokenReset } = data;
+        dispatch(setTokenReset(tokenReset));
+
+        toast.success("Tatutan reset password terkirim ke email anda");
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            toast.error(`${error?.response?.data?.errors?.msg}`, {
+                duration: 2000,
+            });
+            return;
+        }
+
+        toast.error(`${error?.errors}`, {
+            duration: 2000,
+        });
     }
-
-    toast.error(`${error?.errors}`, {
-      duration: 2000,
-    });
-  }
 };
