5c9ad899b9f125fb05cc7a69f46067bc75f40d10
Davincy Project <bdavincy.bd@gmail.comm>
2023-11-21 14:17:03 +0700

    feat: add registerOTP pages

---
+++ src/pages/Auth/RegisterOtp.jsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
+
+const RegisterOtp = () => {
+    return (
+        <div className="flex min-h-screen">
+            <div className="w-[100%] lg:w-[50%] flex justify-start items-center mx-[23px] lg:px-[145px] ">
+                <form onSubmit="" className="w-full">
+                    <div className="flex items-start gap-2">
+                        <Link to={"/register"}>
+                            <FaArrowLeft className="w-10 mt-[10px]" />
+                        </Link>
+                        <h1 className="text-[24px] font-bold text-[#6148FF] mb-8">
+                            Masukkan OTP
+                        </h1>
+                    </div>
+
+                    <div className="flex flex-col gap-5">
+                        <div className="flex flex-col">
+                            <label className="text-[12px] mb-[4px] text-center">
+                                Ketik 6 digit kode yang dikirimkan ke mail
+                            </label>
+                            <input
+                                type="text"
+                                className="border w-full p-2 rounded-2xl"
+                                placeholder="Kode OTP"
+                                // value={nama}
+                                // onChange={(e) => setNama(e.target.value)}
+                            />
+                        </div>
+                        <div className="flex flex-col">
+                            <label className="text-[12px] mb-[4px] text-center">
+                                Kirim Ulang OTP dalam 60 detik
+                            </label>
+                        </div>
+                    </div>
+                    <button className="w-full text-[14px] font-medium bg-[#6148FF] text-white py-[10px] rounded-2xl mt-5">
+                        Simpan
+                    </button>
+                </form>
+            </div>
+
+            <div className="hidden lg:flex justify-center items-center bg-[#6148FF] w-[50%] min-h-[100dvh]">
+                <img src="/logo.png" alt="logo" />
+            </div>
+        </div>
+    );
+};
+
+export default RegisterOtp;
