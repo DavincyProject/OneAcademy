8f567a74795682ab11cf0bf0e5aa2f728570ebbe
Billy <76769657+DavincyProject@users.noreply.github.com>
2023-11-28 13:27:13 +0700

    Changes (#30)
    * Staging (#6)
    
    * feat: add register, reset password page (#4)
    
    Co-authored-by: Davincy Project <bdavincy.bd@gmail.comm>
    
    * feature: add register toast, reset password pages, and redux
    
    ---------
    
    Co-authored-by: Davincy Project <bdavincy.bd@gmail.comm>
    
    * feat: add registerOTP pages
    
    * feature: add OTP pages for register
    
    * tweaks: add error validation to register pages
    
    * tweaks: add redux for reset password
    
    * fix: push new redux and reset password pages
    
    * fix: fix bug on resetToken
    
    * fix: fix bug on api link for reset password
    
    * fix: fix bug on id reset
    
    * fix: testing reset password v5
    
    * fix: rollback to v4 reset password
    
    * changes: change all api to fetch error from axios
    
    * feat: add details kelas pages
    
    * feat: details for class is 60% done
    
    * feat: details for class is 60% done
    
    * Update ResetPassword.jsx
    
    * Update ResetPassword.jsx
    
    * feat: create otp input for register pages
    
    * feat: add details course pages and video player for courses
    
    * feat: add logic to details player
    
    * tweaks: change background color for video player
    
    * tweaks: add effect to know user play video status
    
    * tweaks: when user click on list course while select video, icon will change
    
    * feat: add button to buy class in details pages
    
    * tweaks: ignore readme.md
    
    * tweaks: add SEO for image and form
    
    * changes: change some code on reset pass
    
    * tweaks: adjust code for videocardlist and video player
    
    * feat: add admin login and admin dashboard page
    
    * tweaks: SEO Optimization on details pages
    
    * changes: changes some code on homepage
    
    ---------
    
    Co-authored-by: Davincy Project <bdavincy.bd@gmail.comm>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,60 +1,37 @@
 import CardCategori from "../components/Card/CardCategori";
 import CardCourse from "../components/Card/CardCourse";
-import Navbar from "../components/Navbar";
 
 const Home = () => {
-  return (
-    <>
-      <div className="navbar bg-base-100">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div>
-        <div className="flex-none gap-2">
-          <div className="form-control">
-            <input
-              type="text"
-              placeholder="Search"
-              className="input input-bordered w-24 md:w-auto"
-            />
-          </div>
-          <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                />
-              </div>
-            </label>
-            <ul
-              tabIndex={0}
-              className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
-            ></ul>
-          </div>
-        </div>
-      </div>
-
-      <div className="mx-auto flex flex-col lg:flex-row bg-[#6148FF]">
-        <div className="lg:w-[60%] ">
-          <img src="/people_dasbord.png" className="w-full lg:w-full " />
-        </div>
-        <div className=" flex  container py-4 lg:w-[40%] ">
-          <div className="flex flex-col justify-center gap-4">
-            <div className="flex flex-col font-Montserrat font-bold  lg:text-2xl text-white gap-3">
-              <h1>Belajar</h1>
-              <h1>dari Praktisi Terbaik!</h1>
+    return (
+        <>
+            <div className="mx-auto flex flex-col lg:flex-row bg-[#6148FF] relative">
+                <div className="lg:w-[60%] relative">
+                    <img
+                        src="/people_dasboard.png"
+                        className="hidden w-full lg:w-full lg:block"
+                        alt="People Dashboard"
+                    />
+                    <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-l from-[#6148FF] via-white to-transparent opacity-80"></div>
+                </div>
+                <div className="flex container justify-center p-4 lg:w-[40%]">
+                    <div className="flex flex-col justify-center gap-4">
+                        <div className="flex flex-col justify-center font-Montserrat font-bold lg:text-2xl text-white gap-3">
+                            <h1 className="text-[24px] font-bold">
+                                Belajar <br /> dari Praktisi Terbaik!
+                            </h1>
+                            <h1></h1>
+                        </div>
+                        <button className="btn bg-white text-[#6148FF] font-Montserrat font-bold text-base rounded-[10px] py-2 w-[100%]">
+                            IKUTI KELAS
+                        </button>
+                    </div>
+                </div>
             </div>
-            <button className="bg-white text-[#6148FF] font-Montserrat font-bold text-base rounded-[10px] py-2 w-[100%]">
-              IKUTI KELAS
-            </button>
-          </div>
-        </div>
-      </div>
-      <CardCategori />
-      <CardCourse />
-      <Navbar />
-    </>
-  );
+
+            <CardCategori />
+            <CardCourse />
+        </>
+    );
 };
 
 export default Home;
