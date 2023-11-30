e893bd6863d7b3aa565809bdc61d9621381e441f
Billy <76769657+DavincyProject@users.noreply.github.com>
2023-11-30 12:36:39 +0700

    Feature (#38)
    * Staging (#3)
    
    * feat: add login UI
    
    * tweaks: fix bug on width
    
    * tweaks: added vercel.json to fix 404 pages vercel
    
    ---------
    
    Co-authored-by: Davincy Project <bdavincy.bd@gmail.comm>
    
    * Staging (#6)
    
    * feat: add register, reset password page (#4)
    
    Co-authored-by: Davincy Project <bdavincy.bd@gmail.comm>
    
    * feature: add register toast, reset password pages, and redux
    
    ---------
    
    Co-authored-by: Davincy Project <bdavincy.bd@gmail.comm>
    
    * feat: add registerOTP pages
    
    * feature: add OTP pages for register
    
    * tweaks: add error validation to register pages
    
    * tweaks: add redux for reset password
    
    * fix: push new redux and reset password pages
    
    * fix: fix bug on resetToken
    
    * fix: fix bug on api link for reset password
    
    * fix: fix bug on id reset
    
    * fix: testing reset password v5
    
    * fix: rollback to v4 reset password
    
    * changes: change all api to fetch error from axios
    
    * feat: add details kelas pages
    
    * feat: details for class is 60% done
    
    * feat: details for class is 60% done
    
    * Update ResetPassword.jsx
    
    * Update ResetPassword.jsx
    
    * feat: create otp input for register pages
    
    * feat: add details course pages and video player for courses
    
    * feat: add logic to details player
    
    * tweaks: change background color for video player
    
    * tweaks: add effect to know user play video status
    
    * tweaks: when user click on list course while select video, icon will change
    
    * feat: add button to buy class in details pages
    
    * tweaks: ignore readme.md
    
    * Update README.md (#23)
    
    * Update README.md (#24)
    
    * tweaks: add SEO for image and form
    
    * changes: change some code on reset pass
    
    * tweaks: adjust code for videocardlist and video player
    
    * feat: add admin login and admin dashboard page
    
    * Update README.md (#26)
    
    * tweaks: SEO Optimization on details pages
    
    * Update README.md (#28)
    
    * changes: changes some code on homepage
    
    * feat: add notification pages
    
    * fix: fix bug toast popup not showing up on reset password
    
    * initial commit
    
    * Update App.jsx
    
    * tweaks: add logic to navbar
    
    * tweaks: change padding on laptop screen
    
    * add navbar
    
    * changes: change Navbar logic
    
    * fix: fix critical bugs on Navbar not detected token from localstorage
    
    * fix: fix critical bugs on Navbar not detected token from localstorage
    
    * feat: add profile setting pages
    
    ---------
    
    Co-authored-by: Davincy Project <bdavincy.bd@gmail.comm>
    Co-authored-by: AditFebriNurHidayat <fnadit16@gmail.com>

--- src/pages/profile/Account.jsx
+++ src/pages/profile/Account.jsx
@@ -1,5 +1,123 @@
+import { useState } from "react";
+import { FaArrowLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import HistoryPembayaran from "../../components/Profile/HistoryPembayaran";
+import EditProfile from "../../components/Profile/EditProfile";
+import UbahPassword from "../../components/Profile/UbahPassword";
+
 const Account = () => {
-    return <div></div>;
+    const [activeLink, setActiveLink] = useState(null);
+
+    const handleClick = (link) => {
+        setActiveLink(link);
+    };
+
+    const handleRenderContent = () => {
+        switch (activeLink) {
+            case "profile":
+                return <EditProfile />;
+            case "ubahPassword":
+                return <UbahPassword />;
+            case "riwayatPembayaran":
+                return <HistoryPembayaran />;
+            default:
+                return null;
+        }
+    };
+
+    return (
+        <div>
+            <div className="bg-[#EBF3FC] h-[250px] relative">
+                <div className="absolute flex justify-center w-full top-[9dvh] ">
+                    <div className="flex flex-col justify-center w-3/4">
+                        <Link
+                            to={"/"}
+                            className="text-[#6148FF] text-[16px] font-bold flex gap-2 items-center mb-5"
+                        >
+                            <FaArrowLeft className="text-[#6148FF]" />
+                            Kembali Ke Beranda
+                        </Link>
+                        <div className="card card-compact w-[80dvw] bg-base-100 shadow-xl mb-5">
+                            <div className="p-2 bg-[#6148FF] rounded-t-md text-center">
+                                <h1 className="text-xl text-white font-bold">
+                                    Akun
+                                </h1>
+                            </div>
+
+                            <div className="w-full card-body min-h-[50dvh] flex md:flex-row">
+                                <div className="w-2/4 flex">
+                                    <ul className="w-2/4">
+                                        <Link
+                                            onClick={() =>
+                                                handleClick("profile")
+                                            }
+                                            className={`text-${
+                                                activeLink === "profile"
+                                                    ? "[#6148FF]"
+                                                    : "black"
+                                            }`}
+                                        >
+                                            <div className="flex items-center gap-2 text-[16px] mb-2 font-bold">
+                                                <img src="/Profile/edit.svg" />
+                                                <h1>Profil Saya</h1>
+                                            </div>
+                                            <div className="border-t mb-2 border-[#E5E5E5]"></div>
+                                        </Link>
+                                        <Link
+                                            onClick={() =>
+                                                handleClick("ubahPassword")
+                                            }
+                                            className={`text-${
+                                                activeLink === "ubahPassword"
+                                                    ? "[#6148FF]"
+                                                    : "black"
+                                            }`}
+                                        >
+                                            <div className="flex items-center gap-2 text-[16px] mb-2 font-bold">
+                                                <img src="/Profile/settings.svg" />
+                                                <h1>Ubah Password</h1>
+                                            </div>
+                                            <div className="border-t mb-2 border-[#E5E5E5]"></div>
+                                        </Link>
+                                        <Link
+                                            onClick={() =>
+                                                handleClick("riwayatPembayaran")
+                                            }
+                                            className={`text-${
+                                                activeLink ===
+                                                "riwayatPembayaran"
+                                                    ? "[#6148FF]"
+                                                    : "black"
+                                            }`}
+                                        >
+                                            <div className="flex items-center gap-2 text-[16px] mb-2 font-bold">
+                                                <img src="/Profile/cart.svg" />
+                                                <h1>Riwayat Pembayaran</h1>
+                                            </div>
+                                            <div className="border-t mb-2 border-[#E5E5E5]"></div>
+                                        </Link>
+                                        <Link className="text-red-600">
+                                            <div className="flex items-center gap-2 text-[16px] mb-2 font-bold">
+                                                <img src="/Profile/log-out.svg" />
+                                                <h1>Keluar</h1>
+                                            </div>
+                                            <div className="border-t mb-2 border-[#E5E5E5]"></div>
+                                        </Link>
+                                        <h1 className="text-center mt-2 text-[#8A8A8A] text-xs">
+                                            Version 1.1.0
+                                        </h1>
+                                    </ul>
+                                </div>
+                                <div className="w-2/4 flex justify-center">
+                                    {handleRenderContent()}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Account;
