563186a0b1b34bd286bd21eddbe99b2999fb5433
Billy <76769657+DavincyProject@users.noreply.github.com>
2023-12-03 18:06:06 +0700

    Fix Bugs (#46)
    * Staging (#3)
    
    * feat: add login UI
    
    * tweaks: fix bug on width
    
    * tweaks: added vercel.json to fix 404 pages vercel
    
    ---------
    
    Co-authored-by: Davincy Project <bdavincy.bd@gmail.comm>
    
    * Staging (#6)
    
    * feat: add register, reset password page (#4)
    
    Co-authored-by: Davincy Project <bdavincy.bd@gmail.comm>
    
    * feature: add register toast, reset password pages, and redux
    
    ---------
    
    Co-authored-by: Davincy Project <bdavincy.bd@gmail.comm>
    
    * feat: add registerOTP pages
    
    * feature: add OTP pages for register
    
    * tweaks: add error validation to register pages
    
    * tweaks: add redux for reset password
    
    * fix: push new redux and reset password pages
    
    * fix: fix bug on resetToken
    
    * fix: fix bug on api link for reset password
    
    * fix: fix bug on id reset
    
    * fix: testing reset password v5
    
    * fix: rollback to v4 reset password
    
    * changes: change all api to fetch error from axios
    
    * feat: add details kelas pages
    
    * feat: details for class is 60% done
    
    * feat: details for class is 60% done
    
    * Update ResetPassword.jsx
    
    * Update ResetPassword.jsx
    
    * feat: create otp input for register pages
    
    * feat: add details course pages and video player for courses
    
    * feat: add logic to details player
    
    * tweaks: change background color for video player
    
    * tweaks: add effect to know user play video status
    
    * tweaks: when user click on list course while select video, icon will change
    
    * feat: add button to buy class in details pages
    
    * tweaks: ignore readme.md
    
    * Update README.md (#23)
    
    * Update README.md (#24)
    
    * tweaks: add SEO for image and form
    
    * changes: change some code on reset pass
    
    * tweaks: adjust code for videocardlist and video player
    
    * feat: add admin login and admin dashboard page
    
    * Update README.md (#26)
    
    * tweaks: SEO Optimization on details pages
    
    * Update README.md (#28)
    
    * changes: changes some code on homepage
    
    * feat: add notification pages
    
    * fix: fix bug toast popup not showing up on reset password
    
    * initial commit
    
    * Update App.jsx
    
    * tweaks: add logic to navbar
    
    * tweaks: change padding on laptop screen
    
    * add navbar
    
    * changes: change Navbar logic
    
    * fix: fix critical bugs on Navbar not detected token from localstorage
    
    * fix: fix critical bugs on Navbar not detected token from localstorage
    
    * feat: add profile setting pages
    
    * feat: create update profile component
    
    * tweaks: change search height to match tablet responsive
    
    * feat: add profile redux
    
    * feat: add component UpdatePassword for users settings
    
    * feat: add component UpdatePassword for users settings
    
    * tweaks: add Play Video icon on video player to match figma design
    
    * tweaks: add logic for resend otp
    
    * fix: fix bug on card homepages text size
    
    * tweaks: adjust payment pages
    
    * feat: add payment history pages
    
    * changes: change card preview course, buy course
    
    * tweaks: change card on filter pages
    
    * fix: fix bugs on home course card
    
    * rebase
    
    * feat: redesign Filter Course pages
    
    * tweaks: fix bugs on Filter pages
    
    ---------
    
    Co-authored-by: Davincy Project <bdavincy.bd@gmail.comm>
    Co-authored-by: AditFebriNurHidayat <fnadit16@gmail.com>

--- src/components/Card/Filter.jsx
+++ src/components/Card/Filter.jsx
@@ -2,7 +2,7 @@ const Filter = () => {
     const checkColor = `[--chkbg:theme(colors.check.100)] [--chkfg:white]`;
     return (
         <div>
-            <div className="card w-full max-w-[300px] bg-base-100  text-center">
+            <div className="card w-full min-w-[280px] bg-base-100 text-center">
                 <div className="card-body">
                     <h2 className="card-title text-[16px] font-bold">Filter</h2>
                     <div className="text-left text-[#202244CC] font-medium">
