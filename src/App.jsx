84a148598bc328e4ebbb098e91bba92d7a5a51cd
Billy <76769657+DavincyProject@users.noreply.github.com>
2023-12-04 20:06:41 +0700

    Tweaks (#49)
    * Staging (#3)
    
    * feat: add login UI
    
    * tweaks: fix bug on width
    
    * tweaks: added vercel.json to fix 404 pages vercel
    
    ---------
    
    Co-authored-by: Davincy Project <bdavincy.bd@gmail.comm>
    
    * Staging (#6)
    
    * feat: add register, reset password page (#4)
    
    Co-authored-by: Davincy Project <bdavincy.bd@gmail.comm>
    
    * feature: add register toast, reset password pages, and redux
    
    ---------
    
    Co-authored-by: Davincy Project <bdavincy.bd@gmail.comm>
    
    * feat: add registerOTP pages
    
    * feature: add OTP pages for register
    
    * tweaks: add error validation to register pages
    
    * tweaks: add redux for reset password
    
    * fix: push new redux and reset password pages
    
    * fix: fix bug on resetToken
    
    * fix: fix bug on api link for reset password
    
    * fix: fix bug on id reset
    
    * fix: testing reset password v5
    
    * fix: rollback to v4 reset password
    
    * changes: change all api to fetch error from axios
    
    * feat: add details kelas pages
    
    * feat: details for class is 60% done
    
    * feat: details for class is 60% done
    
    * Update ResetPassword.jsx
    
    * Update ResetPassword.jsx
    
    * feat: create otp input for register pages
    
    * feat: add details course pages and video player for courses
    
    * feat: add logic to details player
    
    * tweaks: change background color for video player
    
    * tweaks: add effect to know user play video status
    
    * tweaks: when user click on list course while select video, icon will change
    
    * feat: add button to buy class in details pages
    
    * tweaks: ignore readme.md
    
    * Update README.md (#23)
    
    * Update README.md (#24)
    
    * tweaks: add SEO for image and form
    
    * changes: change some code on reset pass
    
    * tweaks: adjust code for videocardlist and video player
    
    * feat: add admin login and admin dashboard page
    
    * Update README.md (#26)
    
    * tweaks: SEO Optimization on details pages
    
    * Update README.md (#28)
    
    * changes: changes some code on homepage
    
    * feat: add notification pages
    
    * fix: fix bug toast popup not showing up on reset password
    
    * initial commit
    
    * Update App.jsx
    
    * tweaks: add logic to navbar
    
    * tweaks: change padding on laptop screen
    
    * add navbar
    
    * changes: change Navbar logic
    
    * fix: fix critical bugs on Navbar not detected token from localstorage
    
    * fix: fix critical bugs on Navbar not detected token from localstorage
    
    * feat: add profile setting pages
    
    * feat: create update profile component
    
    * tweaks: change search height to match tablet responsive
    
    * feat: add profile redux
    
    * feat: add component UpdatePassword for users settings
    
    * feat: add component UpdatePassword for users settings
    
    * tweaks: add Play Video icon on video player to match figma design
    
    * tweaks: add logic for resend otp
    
    * fix: fix bug on card homepages text size
    
    * tweaks: adjust payment pages
    
    * feat: add payment history pages
    
    * changes: change card preview course, buy course
    
    * tweaks: change card on filter pages
    
    * fix: fix bugs on home course card
    
    * rebase
    
    * feat: redesign Filter Course pages
    
    * tweaks: fix bugs on Filter pages
    
    * tweaks: integrated Category API to Homepages
    
    * fix: fix link to class
    
    * changes: add modal button for kursus populer responsive
    
    * tweaks: change card buy button on CardCourse Component
    
    * tweaks: change card buy button on CardCourse Component
    
    * tweaks: change hover color on button filter
    
    * tweaks: change width on homescreen
    
    ---------
    
    Co-authored-by: Davincy Project <bdavincy.bd@gmail.comm>
    Co-authored-by: AditFebriNurHidayat <fnadit16@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ import Navbar from "./components/Navbar";
 import AdminLogin from "./pages/Admin/AdminLogin";
 import AdminDashboard from "./pages/Admin/AdminDashboard";
 import Notification from "./pages/profile/Notification";
-// import ClassMe from "./pages/Course/ClassMe";
 import { Toaster } from "react-hot-toast";
 import Protect from "./components/Auth/Protect";
 import PaymentSucces from "./pages/PaymentSucces";
@@ -21,85 +20,77 @@ import Account from "./pages/profile/Account";
 import Payment from "./pages/Payment";
 import Class from "./pages/Course/Class";
 function App() {
-    return (
-        <>
-            <Provider store={store}>
-                <BrowserRouter>
-                    <Toaster position="bottom-center" reverseOrder={false} />
-                    <Navbar />
-                    <Routes>
-                        {/* Global routes */}
-                        <Route path="/" element={<Home />} />
-                        <Route path="/details/:id" element={<KelasDetail />} />
-                        <Route
-                            path="/notification"
-                            element={<Notification />}
-                        />
-                        <Route path="/class" element={<Class />} />
-                        <Route path="/succes" element={<PaymentSucces />} />
-                        <Route path="/account" element={<Account />} />
-                        <Route path="/payment" element={<Payment />} />
+  return (
+    <>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Toaster position="bottom-center" reverseOrder={false} />
+          <Navbar />
+          <Routes>
+            {/* Global routes */}
+            <Route path="/" element={<Home />} />
+            <Route path="/details/:id" element={<KelasDetail />} />
+            <Route path="/notification" element={<Notification />} />
+            <Route path="/class" element={<Class />} />
+            <Route path="/succes" element={<PaymentSucces />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/payment" element={<Payment />} />
 
-                        {/* User Authorization */}
-                        <Route
-                            path="/login"
-                            element={
-                                <Protect>
-                                    <Login />
-                                </Protect>
-                            }
-                        />
-                        <Route
-                            path="/register"
-                            element={
-                                <Protect>
-                                    <Register />
-                                </Protect>
-                            }
-                        />
-                        <Route
-                            path="/validate"
-                            element={
-                                <Protect>
-                                    <RegisterOtp />
-                                </Protect>
-                            }
-                        />
+            {/* User Authorization */}
+            <Route
+              path="/login"
+              element={
+                <Protect>
+                  <Login />
+                </Protect>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <Protect>
+                  <Register />
+                </Protect>
+              }
+            />
+            <Route
+              path="/validate"
+              element={
+                <Protect>
+                  <RegisterOtp />
+                </Protect>
+              }
+            />
 
-                        {/* Reset Password */}
-                        <Route
-                            path="/reset"
-                            element={
-                                <Protect>
-                                    <SendReset />
-                                </Protect>
-                            }
-                        />
-                        <Route
-                            path="/forgot/:id"
-                            element={
-                                <Protect>
-                                    <ResetPassword />
-                                </Protect>
-                            }
-                        />
+            {/* Reset Password */}
+            <Route
+              path="/reset"
+              element={
+                <Protect>
+                  <SendReset />
+                </Protect>
+              }
+            />
+            <Route
+              path="/forgot/:id"
+              element={
+                <Protect>
+                  <ResetPassword />
+                </Protect>
+              }
+            />
 
-                        {/* Admin Authorization */}
-                        <Route path="/admin" element={<AdminLogin />} />
-                        <Route
-                            path="/admin/dashboard"
-                            element={<AdminDashboard />}
-                        />
+            {/* Admin Authorization */}
+            <Route path="/admin" element={<AdminLogin />} />
+            <Route path="/admin/dashboard" element={<AdminDashboard />} />
 
-                        {/* Handle Pages Not Found */}
-                        <Route path="*" element={<NotFound />} />
-
-                        {/* <Route path="/classMe" element={<ClassMe />} /> */}
-                    </Routes>
-                </BrowserRouter>
-            </Provider>
-        </>
-    );
+            {/* Handle Pages Not Found */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </>
+  );
 }
 
 export default App;
--- src/components/Card/CardCourse.jsx
+++ src/components/Card/CardCourse.jsx
@@ -7,172 +7,152 @@ import { BiCategory } from "react-icons/bi";
 // This card for HomePages
 
 const CardCourse = () => {
-    const [selectId, setSelectId] = useState(null);
+  const [selectId, setSelectId] = useState(null);
 
-    const handleBadgeClick = (id) => {
-        setSelectId(id);
-    };
-    return (
-        <div className="flex justify-center">
-            <div className="flex flex-col container gap-5 pt-[26px] pb-[53px] ">
-                <div className="flex flex-row container justify-between px-6">
-                    <h2 className="text-xl font-bold">Kursus Populer</h2>
-                    <Link
-                        to="/class"
-                        className="font-extrabold text-xs text-[#6148FF]"
-                    >
-                        Lihat Semua
-                    </Link>
-                </div>
-
-                <div className="hidden container sm:flex gap-3 justify-center items-center px-6 max-md:flex-wrap max-md:justify-center md:px-0">
-                    {populer.map((datas) => (
-                        <div key={datas.id}>
-                            <div
-                                className={`badge hover:cursor-pointer ${
-                                    selectId === datas.id
-                                        ? "badge-darkblue"
-                                        : "badge-lightwhite"
-                                } font-bold p-5`}
-                                onClick={() => handleBadgeClick(datas.id)}
-                            >
-                                {datas.popular}
-                            </div>
-                        </div>
-                    ))}
-                </div>
-
-                <div className="sm:hidden container px-5">
-                    {/* You can open the modal using document.getElementById('ID').showModal() method */}
-                    <button
-                        className="btn w-full bg-[#6148FF] text-white hover:bg-[#402eb4]"
-                        onClick={() =>
-                            document.getElementById("category").showModal()
-                        }
-                    >
-                        <BiCategory size={20} />
-                        Select Category
-                    </button>
-                    <dialog id="category" className="modal">
-                        <div className="modal-box">
-                            <form method="dialog">
-                                {/* if there is a button in form, it will close the modal */}
-                                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-                                    ✕
-                                </button>
-                            </form>
-
-                            <h3 className="font-bold text-lg">
-                                Select Category
-                            </h3>
-                            <p className="py-3 flex flex-col gap-2">
-                                {populer.map((datas) => (
-                                    <div key={datas.id}>
-                                        <div
-                                            className={`badge w-full hover:cursor-pointer ${
-                                                selectId === datas.id
-                                                    ? "badge-darkblue"
-                                                    : "badge-lightwhite"
-                                            } font-bold p-5`}
-                                            onClick={() =>
-                                                handleBadgeClick(datas.id)
-                                            }
-                                        >
-                                            {datas.popular}
-                                        </div>
-                                    </div>
-                                ))}
-                            </p>
-                        </div>
-                    </dialog>
-                </div>
-
-                <div className="flex gap-3 justify-center md:justify-around items-center flex-wrap px-5">
-                    {course.map((data) => (
-                        <Link
-                            to={`/details/${data.id}`}
-                            key={data.id}
-                            className="shadow-md bg-white flex flex-col flex-grow sm:flex-none items-stretch pb-2.5 rounded-2xl max-w-[323px]"
-                        >
-                            <img
-                                loading="lazy"
-                                srcSet={data.image}
-                                className="aspect-[4.04] w-full overflow-hidden h-[85px] object-cover rounded-t-2xl"
-                            />
-                            <div className="flex w-full flex-col mt-1.5 px-2.5">
-                                <div className="items-stretch self-stretch flex w-full justify-between gap-5">
-                                    <div className="text-indigo-600 text-xs font-bold leading-4 flex-1">
-                                        {data.nama}
-                                    </div>
-                                    <div className="justify-between items-stretch flex gap-0 pl-20 max-md:pl-5">
-                                        <img
-                                            loading="lazy"
-                                            src="/icon/ic_round-star.svg"
-                                        />
-                                        <div className="text-indigo-950 text-xs font-semibold leading-4 grow whitespace-nowrap">
-                                            {data.rating}
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="self-stretch text-black text-xs font-bold leading-4">
-                                    <span className="font-bold text-indigo-950">
-                                        {data.topic}
-                                        <br />
-                                    </span>
-                                    <span className=" text-black">
-                                        {data.mentor}
-                                    </span>
-                                </div>
-                                <div className="items-stretch self-stretch flex justify-between gap-0">
-                                    <div className="flex gap-1 items-center">
-                                        <img
-                                            loading="lazy"
-                                            src="/icon/mdi_badge-outline.svg"
-                                            className="aspect-square object-contain object-center w-3 overflow-hidden shrink-0 max-w-full"
-                                        />
-                                        <div className="text-indigo-600 text-xs font-semibold leading-4 self-stretch">
-                                            {data.level}
-                                        </div>
-                                    </div>
-                                    <div className="flex gap-1 items-center">
-                                        <img
-                                            loading="lazy"
-                                            src="/icon/clarity_book-line.svg"
-                                            className="aspect-square object-contain object-center w-3 overflow-hidden shrink-0 max-w-full"
-                                        />
-                                        <div className="text-black text-xs font-normal leading-4 self-stretch">
-                                            {data.modul}
-                                        </div>
-                                    </div>
-                                    <div className="flex gap-1 items-center">
-                                        <img
-                                            loading="lazy"
-                                            src="/icon/ri_time-fill.svg"
-                                            className="aspect-square object-contain object-center w-3.5 overflow-hidden shrink-0 max-w-full"
-                                        />
-                                        <div className="text-black text-xs font-normal leading-4 self-stretch">
-                                            {data.durasi}
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="badge badge-blue p-3 mt-2">
-                                    <div className="flex gap-1 items-center">
-                                        <img
-                                            src="/icon/Diamond.svg"
-                                            alt="diamond icon"
-                                        />
-                                        <p className="font-bold text-xs">
-                                            {data.harga}
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </Link>
-                    ))}
-                </div>
-            </div>
+  const handleBadgeClick = (id) => {
+    setSelectId(id);
+  };
+  return (
+    <div className="flex justify-center">
+      <div className="flex flex-col justify-center items-center max-w-[1060px] container gap-5 pt-[26px] pb-[53px] ">
+        <div className="flex flex-row container justify-between px-6">
+          <h2 className="text-xl font-bold">Kursus Populer</h2>
+          <Link to="/class" className="font-extrabold text-xs text-[#6148FF]">
+            Lihat Semua
+          </Link>
         </div>
-    );
+
+        <div className="hidden container sm:flex gap-3 justify-center items-center max-md:flex-wrap max-md:justify-center md:px-0">
+          {populer.map((datas) => (
+            <div key={datas.id}>
+              <div
+                className={`badge hover:cursor-pointer text-xs text-center  ${
+                  selectId === datas.id ? "badge-darkblue" : "badge-lightwhite"
+                } font-bold p-5`}
+                onClick={() => handleBadgeClick(datas.id)}
+              >
+                {datas.popular}
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="sm:hidden container px-5">
+          {/* You can open the modal using document.getElementById('ID').showModal() method */}
+          <button
+            className="btn w-full bg-[#6148FF] text-white hover:bg-[#402eb4]"
+            onClick={() => document.getElementById("category").showModal()}
+          >
+            <BiCategory size={20} />
+            Select Category
+          </button>
+          <dialog id="category" className="modal">
+            <div className="modal-box">
+              <form method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                  ✕
+                </button>
+              </form>
+
+              <h3 className="font-bold text-lg">Select Category</h3>
+              <p className="py-3 flex flex-col gap-2">
+                {populer.map((datas) => (
+                  <div key={datas.id}>
+                    <div
+                      className={`badge w-full hover:cursor-pointer ${
+                        selectId === datas.id
+                          ? "badge-darkblue"
+                          : "badge-lightwhite"
+                      } font-bold p-5`}
+                      onClick={() => handleBadgeClick(datas.id)}
+                    >
+                      {datas.popular}
+                    </div>
+                  </div>
+                ))}
+              </p>
+            </div>
+          </dialog>
+        </div>
+
+        <div className="flex gap-6 justify-center md:justify-around items-center flex-wrap px-5">
+          {course.map((data) => (
+            <Link
+              to={`/details/${data.id}`}
+              key={data.id}
+              className="shadow-md bg-white flex flex-col flex-grow sm:flex-none items-stretch pb-2.5 rounded-2xl max-w-[323px]"
+            >
+              <img
+                loading="lazy"
+                srcSet={data.image}
+                className="aspect-[4.04] w-full overflow-hidden h-[85px] object-cover rounded-t-2xl"
+              />
+              <div className="flex w-full flex-col mt-1.5 px-2.5">
+                <div className="items-stretch self-stretch flex w-full justify-between gap-5">
+                  <div className="text-indigo-600 text-xs font-bold leading-4 flex-1">
+                    {data.nama}
+                  </div>
+                  <div className="justify-between items-stretch flex gap-0 pl-20 max-md:pl-5">
+                    <img loading="lazy" src="/icon/ic_round-star.svg" />
+                    <div className="text-indigo-950 text-xs font-semibold leading-4 grow whitespace-nowrap">
+                      {data.rating}
+                    </div>
+                  </div>
+                </div>
+                <div className="self-stretch text-black text-xs font-bold leading-4">
+                  <span className="font-bold text-indigo-950">
+                    {data.topic}
+                    <br />
+                  </span>
+                  <span className=" text-black">{data.mentor}</span>
+                </div>
+                <div className="items-stretch self-stretch flex justify-between gap-0">
+                  <div className="flex gap-1 items-center">
+                    <img
+                      loading="lazy"
+                      src="/icon/mdi_badge-outline.svg"
+                      className="aspect-square object-contain object-center w-3 overflow-hidden shrink-0 max-w-full"
+                    />
+                    <div className="text-indigo-600 text-xs font-semibold leading-4 self-stretch">
+                      {data.level}
+                    </div>
+                  </div>
+                  <div className="flex gap-1 items-center">
+                    <img
+                      loading="lazy"
+                      src="/icon/clarity_book-line.svg"
+                      className="aspect-square object-contain object-center w-3 overflow-hidden shrink-0 max-w-full"
+                    />
+                    <div className="text-black text-xs font-normal leading-4 self-stretch">
+                      {data.modul}
+                    </div>
+                  </div>
+                  <div className="flex gap-1 items-center">
+                    <img
+                      loading="lazy"
+                      src="/icon/ri_time-fill.svg"
+                      className="aspect-square object-contain object-center w-3.5 overflow-hidden shrink-0 max-w-full"
+                    />
+                    <div className="text-black text-xs font-normal leading-4 self-stretch">
+                      {data.durasi}
+                    </div>
+                  </div>
+                </div>
+                <div className="badge badge-blue p-3 mt-2">
+                  <div className="flex gap-1 items-center font-Poppins font-bold">
+                    <img src="/icon/Diamond.svg" alt="diamond icon" />
+                    <span className="mr-2">Beli</span> Rp {""}
+                    {data.harga}
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CardCourse;
