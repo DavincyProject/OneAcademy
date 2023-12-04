5d75a375c4fef84a3e1be814c25b80a33f637a52
Davincy Project <bdavincy.bd@gmail.com>
2023-12-04 19:40:46 +0700

    tweaks: change card buy button on CardCourse Component

--- src/components/Card/CardCategori.jsx
+++ src/components/Card/CardCategori.jsx
@@ -4,49 +4,49 @@ import { Link } from "react-router-dom";
 import { listCategory } from "../../redux/actions/categoryActions";
 
 const CardCategori = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const categoryList = useSelector((state) => state.category.listCategory);
+  const categoryList = useSelector((state) => state.category.listCategory);
 
-    useEffect(() => {
-        dispatch(listCategory());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(listCategory());
+  }, [dispatch]);
 
-    return (
-        <div className="container-fluid flex justify-center bg-[#EBF3FC]">
-            <div className="flex w-full flex-col pt-[26px] pb-[14px] gap-5 container">
-                <div className="flex justify-between container px-6">
-                    <h2 className="text-xl font-bold">Kategori Belajar</h2>
-                    <Link
-                        to="/class"
-                        className=" font-extrabold text-xs max-w-fit text-[#6148FF]"
-                    >
-                        Lihat Semua
-                    </Link>
-                </div>
-                <div className="flex flex-col flex-wrap">
-                    <div className="justify-between flex flex-wrap grow gap-2.5">
-                        {categoryList.map((category) => (
-                            <div
-                                key={category.id}
-                                className="justify-center items-center flex grow basis-[0%] flex-col pl-1.5 pr-2.5"
-                            >
-                                <img
-                                    loading="lazy"
-                                    alt={category.name}
-                                    src={category.imageId}
-                                    className="aspect-[1.4] object-contain object-center w-[140px] "
-                                />
-                                <div className="text-black text-center text-xs font-semibold leading-9 whitespace-nowrap">
-                                    {category.name}
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="flex justify-center bg-[#EBF3FC]">
+      <div className="flex w-full justify-center items-center max-w-[1200px] flex-col pt-[26px] pb-[14px] gap-5 container">
+        <div className="flex justify-between container px-6">
+          <h2 className="text-xl font-bold">Kategori Belajar</h2>
+          <Link
+            to="/class"
+            className=" font-extrabold text-xs max-w-fit text-[#6148FF]"
+          >
+            Lihat Semua
+          </Link>
         </div>
-    );
+        <div className="flex flex-col flex-wrap max-w-[1200px] px-6">
+          <div className="justify-between flex flex-wrap grow gap-2.5">
+            {categoryList.map((category) => (
+              <div
+                key={category.id}
+                className="justify-center items-center flex grow basis-[0%] flex-col pl-1.5 pr-2.5"
+              >
+                <img
+                  loading="lazy"
+                  alt={category.name}
+                  src={category.image.url}
+                  className="aspect-[1.6] object-cover object-center w-[140px] rounded-xl"
+                />
+                <div className="text-black text-center text-xs font-semibold leading-9 whitespace-nowrap">
+                  {category.name}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CardCategori;
