1f50ffe40fc188375ae6daf1f6dd10b17d72ca7e
Davincy Project <bdavincy.bd@gmail.com>
2023-12-13 20:16:19 +0700

    fix: fix bug on register data

--- src/pages/Admin/KelolaChapter.jsx
+++ src/pages/Admin/KelolaChapter.jsx
@@ -5,27 +5,34 @@ import { detailsCourse } from "../../redux/actions/courseActions";
 import { useParams } from "react-router-dom";
 import UpdateChapter from "../../components/Admin/chapters/UpdateChapter";
 import { Link } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
 
 const KelolaChapter = () => {
-    const { id } = useParams();
-    const { courseDetails } = useSelector((state) => state.course);
-    const dispatch = useDispatch();
+  const { id } = useParams();
+  const { courseDetails } = useSelector((state) => state.course);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(detailsCourse(id));
-    }, [dispatch, id]);
+  useEffect(() => {
+    dispatch(detailsCourse(id));
+  }, [dispatch, id]);
 
-    return (
-        <div>
-            <Link to={"/admin/dashboard"}>Kembali</Link>
-            <div className="card">
-                <UpdateChapter details={courseDetails} />
-            </div>
-        </div>
-    );
+  return (
+    <div className="container-fluid p-5">
+      <Link
+        to={"/admin/dashboard"}
+        className="text-darkblue text-[16px] font-bold flex gap-2 items-center mb-5"
+      >
+        <FaArrowLeft className="text-darkblue" />
+        Kembali Ke Dashboard
+      </Link>
+      <div className="container">
+        <UpdateChapter details={courseDetails} />
+      </div>
+    </div>
+  );
 };
 
 export default KelolaChapter;
 KelolaChapter.propTypes = {
-    id: Proptype.node,
+  id: Proptype.node,
 };
--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -7,163 +7,163 @@ import toast from "react-hot-toast";
 export const protecttoken = () => async () => {};
 
 export const login = (email, password, navigate) => async (dispatch) => {
-    try {
-        const response = await axios.post(ENDPOINTS.login, {
-            email,
-            password,
-        });
+  try {
+    const response = await axios.post(ENDPOINTS.login, {
+      email,
+      password,
+    });
 
-        const { data } = response.data;
-        const { token } = data;
+    const { data } = response.data;
+    const { token } = data;
 
-        dispatch(setToken(token));
-        navigate("/");
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            toast.error(`${error?.response?.data?.error}`, {
-                duration: 2000,
-            });
-            return;
-        }
-        toast.error(`${error?.data?.error}`, {
-            duration: 2000,
-        });
+    dispatch(setToken(token));
+    navigate("/");
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      toast.error(`${error?.response?.data?.error}`, {
+        duration: 2000,
+      });
+      return;
     }
+    toast.error(`${error?.data?.error}`, {
+      duration: 2000,
+    });
+  }
 };
 
 export const register =
-    (name, email, phone, password, navigate) => async () => {
-        try {
-            const response = await axios.post(ENDPOINTS.register, {
-                name,
-                email,
-                phone,
-                password,
-                roleId: 2,
-            });
+  (email, password, name, phone, navigate) => async () => {
+    try {
+      const response = await axios.post(ENDPOINTS.register, {
+        email,
+        name,
+        phone,
+        password,
+        roleId: 2,
+      });
 
-            const { message, user } = response.data;
+      const { message, user } = response.data;
 
-            toast.success(message);
-            localStorage.setItem("email", user.email);
+      toast.success(message, { duration: 1500 });
+      localStorage.setItem("email", user.email);
 
-            setTimeout(() => {
-                navigate("/validate");
-            }, 2000);
-        } catch (error) {
-            if (axios.isAxiosError(error)) {
-                toast.error(`${error?.response?.data?.message}`, {
-                    duration: 2000,
-                });
-                return;
-            }
-            toast.error(`${error?.error}`, {
-                duration: 2000,
-            });
-        }
-    };
+      setTimeout(() => {
+        navigate("/validate");
+      }, 2000);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        toast.error(`${error?.response?.data?.message}`, {
+          duration: 1500,
+        });
+        return;
+      }
+      toast.error(`${error?.error}`, {
+        duration: 2000,
+      });
+    }
+  };
 
 export const activateAccount = (OTP, navigate) => async () => {
-    try {
-        const email = localStorage.getItem("email");
+  try {
+    const email = localStorage.getItem("email");
 
-        const response = await axios.post(ENDPOINTS.activateaccount, {
-            email,
-            OTP,
-        });
+    const response = await axios.post(ENDPOINTS.activateaccount, {
+      email,
+      OTP,
+    });
 
-        const { message } = response.data;
+    const { message } = response.data;
 
-        toast.success(message);
-        localStorage.removeItem("email");
+    toast.success(message);
+    localStorage.removeItem("email");
 
-        setTimeout(() => {
-            navigate("/login");
-        }, 2000);
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            toast.error(`${error?.response?.data?.message}`, {
-                duration: 2000,
-            });
-        } else {
-            toast.error(`${error?.message}`, {
-                duration: 2000,
-            });
-        }
+    setTimeout(() => {
+      navigate("/login");
+    }, 2000);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      toast.error(`${error?.response?.data?.message}`, {
+        duration: 2000,
+      });
+    } else {
+      toast.error(`${error?.error}`, {
+        duration: 2000,
+      });
     }
+  }
 };
 
 export const resendOtp = () => async () => {
-    try {
-        const email = localStorage.getItem("email");
+  try {
+    const email = localStorage.getItem("email");
 
-        await axios.post(ENDPOINTS.resendotp, {
-            email,
-        });
+    await axios.post(ENDPOINTS.resendotp, {
+      email,
+    });
 
-        toast.success("Otp berhasil dikirim");
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            toast.error(`${error?.response?.data?.message}`, {
-                duration: 2000,
-            });
-            return;
-        }
-        toast.error(`${error?.data?.error}`, {
-            duration: 2000,
-        });
+    toast.success("Otp berhasil dikirim");
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      toast.error(`${error?.response?.data?.message}`, {
+        duration: 2000,
+      });
+      return;
     }
+    toast.error(`${error?.data?.error}`, {
+      duration: 2000,
+    });
+  }
 };
 
 export const resetPassword = (email) => async () => {
-    try {
-        const response = await axios.post(ENDPOINTS.resetpassword, {
-            email,
-        });
+  try {
+    const response = await axios.post(ENDPOINTS.resetpassword, {
+      email,
+    });
 
-        const { message } = response.data;
-        toast.success(message);
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            toast.error(`${error?.response?.data?.message}`, {
-                duration: 2000,
-            });
-            return;
-        }
-        toast.error(`${error?.data?.error}`, {
-            duration: 2000,
-        });
+    const { message } = response.data;
+    toast.success(message);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      toast.error(`${error?.response?.data?.message}`, {
+        duration: 2000,
+      });
+      return;
     }
+    toast.error(`${error?.data?.error}`, {
+      duration: 2000,
+    });
+  }
 };
 
 export const forgotPassword = (password, id, navigate) => async () => {
-    try {
-        const response = await axios.post(ENDPOINTS.setpassword, {
-            id,
-            password,
-        });
+  try {
+    const response = await axios.post(ENDPOINTS.setpassword, {
+      id,
+      password,
+    });
 
-        const { message } = response.data;
-        toast.success(message);
+    const { message } = response.data;
+    toast.success(message);
 
-        setTimeout(() => {
-            navigate("/login");
-        }, 1500);
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            toast.error(`${error?.response?.data?.message}`, {
-                duration: 2000,
-            });
-            return;
-        }
-        toast.error(`${error?.data?.error}`, {
-            duration: 2000,
-        });
+    setTimeout(() => {
+      navigate("/login");
+    }, 1500);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      toast.error(`${error?.response?.data?.message}`, {
+        duration: 2000,
+      });
+      return;
     }
+    toast.error(`${error?.data?.error}`, {
+      duration: 2000,
+    });
+  }
 };
 
 export const logout = (navigate) => (dispatch) => {
-    localStorage.removeItem("token");
-    dispatch(setToken(null));
-    navigate("/login");
+  localStorage.removeItem("token");
+  dispatch(setToken(null));
+  navigate("/login");
 };
