e13fa23d869f13c74043758c89dc89fd80a14b0a
DavincyProject <bdavincy.bd@gmail.com>
2023-12-21 13:40:47 +0700

    Tweaks: add logic when admin login

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -4,49 +4,48 @@ import { ENDPOINTS } from "../../utils/endpointApi";
 import toast from "react-hot-toast";
 import { setProfileData } from "../reducers/profileReducers";
 
-export const login =
-  (email, password, navigate) => async (dispatch, getState) => {
-    try {
-      const response = await axios.post(ENDPOINTS.login, {
-        email,
-        password,
-      });
+export const login = (email, password, navigate) => async (dispatch) => {
+  try {
+    const response = await axios.post(ENDPOINTS.login, {
+      email,
+      password,
+    });
 
-      const { token } = response.data.data;
-      const { id, roleId } = response.data;
+    const { token } = response.data.data;
+    const { id, roleId } = response.data;
 
-      dispatch(setToken(token));
-      dispatch(setIdUser(id));
+    dispatch(setToken(token));
+    dispatch(setIdUser(id));
 
-      localStorage.setItem("idUser", id);
-      localStorage.removeItem("countdown");
+    localStorage.setItem("idUser", id);
+    localStorage.removeItem("countdown");
 
-      if (roleId !== 2) {
-        navigate("/admin/dashboard");
+    if (roleId !== 2) {
+      navigate("/admin/dashboard");
+    } else {
+      navigate("/");
+    }
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (error.response) {
+        const errorMessage = error.response.data.error;
+        toast.error(errorMessage, {
+          duration: 2000,
+        });
       } else {
-        navigate("/");
-      }
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        if (error.response) {
-          const errorMessage = error.response.data.error;
-          toast.error(errorMessage, {
-            duration: 2000,
-          });
-        } else {
-          // Respon tidak diterima dari server
-          toast.error("Error: No response received from the server", {
-            duration: 2000,
-          });
-        }
-      } else {
-        // Kesalahan selain dari Axios
-        toast.error("An unexpected error occurred", {
+        // Respon tidak diterima dari server
+        toast.error("Error: No response received from the server", {
           duration: 2000,
         });
       }
+    } else {
+      // Kesalahan selain dari Axios
+      toast.error("An unexpected error occurred", {
+        duration: 2000,
+      });
     }
-  };
+  }
+};
 
 export const register =
   (email, password, name, phone, navigate) => async () => {
