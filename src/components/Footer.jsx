49606386eee9026d2e108ba1e18ee6fd7e96ef5f
Billy <76769657+DavincyProject@users.noreply.github.com>
2024-01-03 16:22:10 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -87,6 +87,24 @@ const Footer = () => {
                 <SlSocialGithub size={20} /> TehBotolBayu
               </Link>
             </li>
+            <li>
+              <Link
+                to={""}
+                target="_blank"
+                className="flex items-center gap-2 hover:text-blue-400 w-fit"
+              >
+                <SlSocialGithub size={20} /> GALUH RAKA M.
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={""}
+                target="_blank"
+                className="flex items-center gap-2 hover:text-blue-400 w-fit"
+              >
+                <SlSocialGithub size={20} /> M. FATHUR NUR K.
+              </Link>
+            </li>
           </ul>
         </div>
 
